8565b17bd55323682f505b60aaa7e24546186c03
Heylon <heylon.white@my.jcu.edu.au>
2016-05-14 22:28:57 +1000

    Benefactor form draft complete

---
+++ canvas_draw.js
@@ -0,0 +1,52 @@
+// JavaScript Document
+//Sourced from: http://codetheory.in/html5-canvas-drawing-lines-with-smooth-edges/
+
+var canvas = document.querySelector('#paint');
+var ctx = canvas.getContext('2d');
+ 
+var sketch = document.querySelector('#sketch');
+var sketch_style = getComputedStyle(sketch);
+canvas.width = parseInt(sketch_style.getPropertyValue('width'));
+canvas.height = parseInt(sketch_style.getPropertyValue('height'));
+
+var mouse = {x: 0, y: 0};
+var last_mouse = {x: 0, y: 0};
+ 
+/* Mouse Capturing Work */
+canvas.addEventListener('mousemove', function(e) {
+    last_mouse.x = mouse.x;
+    last_mouse.y = mouse.y;
+ 
+    mouse.x = e.pageX - this.offsetLeft;
+    mouse.y = e.pageY - this.offsetTop;
+}, false);
+
+/* Drawing on Paint App */
+ctx.lineWidth = 2;
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.strokeStyle = 'blue';
+ 
+canvas.addEventListener('mousedown', function(e) {
+    ctx.beginPath();
+    ctx.moveTo(mouse.x, mouse.y);
+ 
+    canvas.addEventListener('mousemove', onPaint, false);
+}, false);
+ 
+canvas.addEventListener('mouseup', function() {
+    canvas.removeEventListener('mousemove', onPaint, false);
+}, false);
+ 
+var onPaint = function() {
+    ctx.beginPath();
+    ctx.moveTo(last_mouse.x, last_mouse.y);
+    ctx.lineTo(mouse.x, mouse.y);
+    ctx.closePath();
+    ctx.stroke();
+};
+
+//clear
+function clearCanvas(){
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
