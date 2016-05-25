e75787ca0651f7d4758ea54f35adf0a3511c4053
Heylon <heylon.white@my.jcu.edu.au>
2016-05-26 00:37:12 +1000

    Gallery improvements

--- page_script.js
+++ page_script.js
@@ -49,3 +49,59 @@ function adjust_footer(){
 	//main_page.style.paddingBottom = footer.offsetHeight + "px";
 	//footer.style.bottom = "calc(0% + " +0+ "px)";
 }
+
+//Collapsible sections code
+
+/*To use this code add initalise_collapsible_sections() function call to the bottom of desired webpage
+Also do not use inline-styles on collapsible elements - Heylon
+*/
+function initalise_collapsible_sections(){
+	var collapsible_sections = document.getElementsByClassName('collapsible');
+	var collapsible_buttons = document.getElementsByClassName('collapsiblebutton');
+	for(var i = 0; i < collapsible_sections.length; i++){
+		var button_states = ['show more', 'show less'];
+		var button_text = '';
+		//section
+		if(collapsible_sections.item(i).getAttribute('data-collapsed') == 'true'){
+			//collapsed
+			collapsible_sections.item(i).style.display = 'none';
+			button_text = button_states[0];
+		}else{
+			//found non-collapsed section
+			button_text = button_states[1];
+		}
+		//button
+		for(var j = 0; j < collapsible_buttons.length; j++){
+			if(collapsible_sections.item(i).getAttribute('data-collapsible_id') == collapsible_buttons.item(j).getAttribute('data-collapsible_id')){
+				//Button matched to section
+				collapsible_buttons.item(j).innerHTML = button_text;
+				collapsible_buttons.item(j).setAttribute('data-collapsible_section_index',i);
+				//Add handler
+				collapsible_buttons.item(j).addEventListener('click', toggle_collapsible_section);
+			}
+		}
+	}
+}
+function toggle_collapsible_section(){
+	var collapsible_sections = document.getElementsByClassName('collapsible');
+	var this_button_id = this.getAttribute('data-collapsible_id');
+	var my_collapsible_section_id = this.getAttribute('data-collapsible_section_index');
+	
+	var button_states = ['show more', 'show less'];
+	
+	if(collapsible_sections.item(my_collapsible_section_id).getAttribute('data-collapsed') == 'true'){
+		//Expand section
+		collapsible_sections.item(my_collapsible_section_id).removeAttribute('style');
+		
+		collapsible_sections.item(my_collapsible_section_id).setAttribute('data-collapsed','false');
+		
+		this.innerHTML = button_states[1];
+	}else{
+		//Collapse section
+		collapsible_sections.item(my_collapsible_section_id).style.display = 'none';
+		
+		collapsible_sections.item(my_collapsible_section_id).setAttribute('data-collapsed','true');
+		
+		this.innerHTML = button_states[0];
+	}
+}
